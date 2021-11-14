c28561d767363d439c957ef3a0671a9109462760
aisyahm <aisyahmulyani01@gmail.com>
2021-11-15 01:02:41 +0700

    Insert migration tables

---
+++ database/migrations/2021_11_14_172443_create_tasks_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('list_id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('name');
+            $table->datetime('reminder_datetime');
+            $table->date('due_date');
+            $table->text('attachment_url')->nullable();
+            $table->text('note')->nullable();
+            $table->boolean('is_complete');
+            $table->boolean('is_my_day');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
