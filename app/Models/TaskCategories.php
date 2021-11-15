ab0fce7020fd4b7f833de97070531017355cee35
aisyahm <aisyahmulyani01@gmail.com>
2021-11-15 13:07:54 +0700

    Insert Model

---
+++ app/Models/TaskCategories.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TaskCategories extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'task_id',
+        'category_id'
+    ];
+
+    // public function tasks()
+    // {
+    //     return $this->belongsTo(Tasks::class);
+    // }
+
+    // public function categories()
+    // {
+    //     return $this->belongsTo(Categories::class);
+    // }
+}
---
+++ app/Models/Tasks.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tasks extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'list_id',
+        'name',
+        'reminder_datetime',
+        'due_date',
+        'attachment_url',
+        'note',
+        'is_complete',
+        'is_my_day'
+    ];
+
+    public function users()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function lists()
+    {
+        return $this->belongsTo(Lists::class);
+    }
+
+    // public function subtasks()
+    // {
+    //     return $this->hasOne(Subtasks::class);
+    // }
+    
+    // public function taskCategories()
+    // {
+    //     return $this->hasMany(TaskCategories::class);
+    // }
+}
